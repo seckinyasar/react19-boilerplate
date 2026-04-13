8d905ee8b0d108e1d316cf8fc873ffa3a889ef8c
seckin <contactseckin@gmail.com>
2026-04-13 18:30:06 +0300

    chore: config file changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typedRoutes: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     qualities: [75, 90, 100],
     remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com", pathname: "/**" },
-      { protocol: "https", hostname: "**.googleusercontent.com", pathname: "/**" },
-      { protocol: "https", hostname: "avatars.githubusercontent.com", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
       { protocol: "https", hostname: "github.com", pathname: "/**" },
       { protocol: "https", hostname: "media.licdn.com", pathname: "/**" },
-      { protocol: "https", hostname: "platform-lookaside.fbsbx.com", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+        pathname: "/**",
+      },
       { protocol: "https", hostname: "cdn.discordapp.com", pathname: "/**" },
       { protocol: "https", hostname: "pbs.twimg.com", pathname: "/**" },
     ],
