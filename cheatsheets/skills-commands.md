ff1322971e98bc44c2fd4c66d2f6db24eadc36ae
seckin <contactseckin@gmail.com>
2026-04-18 22:54:57 +0300

    chore: added global instance for prisma

---
+++ cheatsheets/skills-commands.md
@@ -0,0 +1,1 @@
+# npx skills list
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@root/generated/prisma/client";
 import "dotenv/config";
-
 import { getServerEnv } from "@/env.server";
 
 const connectionString = getServerEnv().DATABASE_URL;
-
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
